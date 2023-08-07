a0be9c1872d21b87971f7959b6d91c039a015da9
Manav <manvashishth19@gmail.com>
2023-08-07 13:20:03 +0530

    Client added

---
+++ client/src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { createTheme } from '@mui/material/styles'
+import { Box, CssBaseline, ThemeProvider } from '@mui/material'
+import {useMemo} from 'react'
+import { themeSettings } from './theme'
+import Navbar from './scenes/navbar'
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);  // empty because need only one time render
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <ThemeProvider theme = {theme}>
+            <CssBaseline/>     {/* removes initail basic css */}
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar/>
+              <Routes>
+                <Route path="/" element={<div>Dashboard page</div>}/>
+                <Route
+                path="/predictions"
+                element={<div>Predictions page</div>}
+                />
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </BrowserRouter>
+      </div>
+      
+  )
+}
+
+export default App
---
+++ client/src/scenes/navbar/index.tsx
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
+import {Box,Typography, useTheme} from '@mui/material'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
