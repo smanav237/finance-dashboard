e1821f5aefff362744adca1999d685425f709a8e
Manav <manvashishth19@gmail.com>
2023-08-12 23:43:49 +0530

    api updated and ROw2 ccompleted

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,19 +1,29 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import {
   GetKpisResponse,
-  
+  GetProductsResponse,
+  GetTransactionsResponse,
 } from "./types";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
-    reducerPath: "main",
-    tagTypes: ["Kpis"],
-    endpoints: (build) => ({
-      getKpis: build.query<Array<GetKpisResponse>, void>({
-        query: () => "kpi/kpis/",
-        providesTags: ["Kpis"],
-      }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["Kpis", "Products", "Transactions"],   // all data from below endpoints is saved here
+  endpoints: (build) => ({
+    getKpis: build.query<Array<GetKpisResponse>, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"],
     }),
-  });
-  
-  export const { useGetKpisQuery } = api;
+    getProducts: build.query<Array<GetProductsResponse>, void>({
+      query: () => "product/products/",
+      providesTags: ["Products"],
+    }),
+    getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+      query: () => "transaction/transactions/",
+      providesTags: ["Transactions"],
+    }),
+  }),
+});
+
+export const { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } =
+  api;
